7e096a91b85dd2ee20f6ca58a979b02e6d3f8aa3
HhTtLllL <1430249706@qq.com>
2020-08-03 20:23:06 +0800

    config comprehend

--- Logger/sylar/config.cpp
+++ Logger/sylar/config.cpp
@@ -24,7 +24,7 @@ static void ListAllMember(const std::string& prefix,
 		 	const YAML::Node& node,
 			std::list<std::pair<std::string, const YAML::Node> >& output){
 	if(prefix.find_first_not_of("abcdefghijklmnopqrstuvwxyz._1234567890")
-			!= std::string::npos){
+			!= std::string::npos){ //find 函数在找不到指定值的情况下 会返回string::npos
 	
 		SYLAR_LOG_ERROR(SYLAR_LOG_ROOT()) << "Config invalid name: " << prefix << " : " << node;
 
@@ -46,8 +46,8 @@ static void ListAllMember(const std::string& prefix,
 }
 
 
+//使用 node 初始化配置模块
 void Config::LoadFromYaml(const YAML::Node& root){
-
 	std::list<std::pair<std::string, const YAML::Node> > all_nodes;
 	ListAllMember("", root, all_nodes);
 
@@ -63,8 +63,9 @@ void Config::LoadFromYaml(const YAML::Node& root){
 		ConfigVarBase::ptr  var = LookupBase(key);
 
 		if(var){
-		
-			if(i.second.IsScalar()){
+			//Scalar  纯量    存量是最基本的、不可再分的值。 以下数据类型都属于 纯量
+																											// 字符串、布尔、整数、浮点数、NULL、时间、日期
+			if(i.second.IsScalar()){   //如果是基本类型就直接转化为 字符串
 				var->fromString(i.second.Scalar());
 			}else{
 				std::stringstream ss;
--- Logger/sylar/config.h
+++ Logger/sylar/config.h
@@ -18,7 +18,11 @@
 
 namespace sylar
 {
+/*
+		ConfigVarBase 是具体的基类，ConfigVar 是一个配置项的具体实现，Config 是配置项的集合
 
+
+*/
 //配置变量的基类
 class ConfigVarBase{
 	
@@ -34,7 +38,7 @@ public:
 
 	virtual ~ConfigVarBase() {}
 
-	//返回配置参数与的名称
+	//返回配置参数的名称
 	const std::string& getName() const { return m_name; }
 	//返回配置参数的描述
 	const std::string& getDescription() const { return m_description; }
@@ -296,12 +300,17 @@ public:
 		return ss.str();
 	}
 
+
 };
 
 
 
 
 //配置项的具体实现类
+//T 是配置项代表的具体类型。 getValue 方法返回对应类型的实例
+// FromStr  实现将string 转化为 T 的功能
+// ToStr: 实现将T 转化为string 的功能
+//
 template<class T, class FromStr = LexicalCast<std::string,T>,
 	class ToStr = LexicalCast<T,std::string> >
 class ConfigVar : public ConfigVarBase{
@@ -309,11 +318,18 @@ class ConfigVar : public ConfigVarBase{
 public:
 	typedef std::shared_ptr<ConfigVar> ptr;
 
+	/*通过参数名，参数值，描述，构造ConfigVar
+	 *name 参数名称有效字符为[0-9,a-z,_.]
+	 *default_value 参数的默认值
+	 *
+	 * description 参数的描述
+	 * */
 	ConfigVar(const std::string& name, const T& default_value,const std::string& description = "")
 		: ConfigVarBase(name,description), m_val(default_value){
 		
 		}
-
+	/*将参数值转换成YAML String 类型,当转换失败抛出异常
+	 * */
 	std::string toString () override{
 	
 		try{
@@ -328,12 +344,15 @@ public:
 		return "";
 	}
 
-
+	/*从YAML String 转成参数的值
+	 * 当转换失败时，抛出异常
+	 * */
 	bool fromString(const std::string& val) override {
 		
 		try{
 			//把string 类型转换为我们需要的类型 模板T 类型
 			//m_val = boost::lexical_cast<T>(val);
+			//class FromStr = lexcal_cast<std::string, T>
 			setValue(FromStr() (val));
 
 		}catch(std::exception& e){
@@ -344,25 +363,36 @@ public:
 
 		return false;
 	}
-
+	//获取当前参数的值
 	const T getValue() const { return m_val; }
+	//设置当前参数 的值
 	void setValue(const T& val) { m_val = val ; } 
 private:
 	T m_val;
-
-
-
-
-
 };
 
 //ConfigVar 的集合类
+//配置集合类，提供所有配置项的ConfigVar 的统一管理功能
+//加载配置文件，更新配置文件，定义配置项等等
 class Config{
 
 public:
 
 	typedef std::map<std::string, ConfigVarBase::ptr> ConfigVarMap;
 
+	/*创建和定义配置项，同一个配置向只能创建一次，否则会冲突
+	 *
+	 *获取/创建对一个参数名的配置参数
+	 *name : 配置参数名称
+	 *default_value 参数默认值
+	 *description  参数描述
+	 *获取参数名为name 的配置参数，如果存在直接返回，如果不存在，创建参数配置并用 default_value 赋值
+	 *
+	 * 返回对应的配置参数，如果参数名存在但是类型不匹配则返回nullptr
+	 *
+	 * 如果参数名包括非法字符，则抛出异常
+	 *
+	 * */
 	template<class T>
 	static typename ConfigVar<T>::ptr Lookup(const std::string& name,
 			const T& default_value, const std::string& description = ""){
@@ -371,19 +401,25 @@ public:
 			SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << "Lookup name" << name << "exists";
 			return tmp;
 		}
-
+		// 检查名称中是否包含非法字符,有则抛出异常
 		if(name.find_first_not_of("abcdefghigklmnopqrstuvwxyz._0123456789") != std::string::npos){
 			SYLAR_LOG_ERROR(SYLAR_LOG_ROOT()) << "Lookup name invalid " << name;
 			throw std::invalid_argument(name);
-		
 		}
 
+		//符合以上条件则 向map容器中插入该值
 		typename ConfigVar<T>::ptr v(new ConfigVar<T>(name, default_value,description));
 		s_datas[name] = v;
 
 		return v;
 	}
-
+	
+	//获取对应名称的配置项，用来获取上面函数创建的配置项
+	/*
+			查找配置参数
+			name 配置参数名称
+			返回配置参数名为name 的配置参数, 没找到则返回 nullptr
+	*/
 	template<class T>
 	static typename ConfigVar<T>::ptr Lookup(const std::string& name){
 		auto it = s_datas.find(name);
@@ -392,32 +428,21 @@ public:
 		}
 
 		return std::dynamic_pointer_cast<ConfigVar<T> >(it->second);
-	
 	}
-
+	//加载配置文件，加载一个目录下面的所有YAML 文件，支持多文件，不同的配置项可以分开文件定义
+	//YAML::Node 初始化配置模块
 	static void LoadFromYaml(const YAML::Node& root);
+	
+	/*查找配置参数,返回配置参数的基类
+	 * name 配置参数名称
+	 *
+	 * */
 	static ConfigVarBase::ptr LookupBase(const std::string& name);
-
-
 
 private:
 	static ConfigVarMap s_datas;
 
 };
 
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
 #endif
